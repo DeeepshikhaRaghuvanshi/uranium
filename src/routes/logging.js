a498bd7dd9789a01af8dc34d8d14621fc866a72f
DeeepshikhaRaghuvanshi <raghuvanshideepshikha@gmail.com>
2022-04-06 20:15:18 +0530

    Assignment

---
+++ src/routes/logging.js
@@ -0,0 +1,11 @@
+let a = "https://www.google.com";
+
+let log = function(){
+
+console.log("Whatsup People !")
+
+}
+
+module.exports.googlie = a;
+module.exports.function = log;
+
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const Deep = require('./logging.js')
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
+
+router.get('/test-me1', function (req, res) {
+    res.send('My second ever api!')
+    console.log('Googlie is : ', Deep.googlie);
+    Deep.function();
+});
+
 
 module.exports = router;
 // adding this comment for no reason
