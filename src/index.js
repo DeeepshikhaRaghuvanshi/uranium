5e50dececb891d0d111dfe3f96d7ce493aca0162
DeeepshikhaRaghuvanshi <ddeepshikha.raghuvanshi@gmail.com>
2022-04-19 21:57:39 +0530

    assignmet

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+const requestIp = require('request-ip');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Deepshikha:Yash%401603@cluster0.cnmul.mongodb.net/Deepshikha?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -16,7 +17,13 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 
 app.use (
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        var today = new Date();
+        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        var dateTime = date+' '+time;
+
+        console.log(dateTime + " , " + requestIp.getClientIp(req) + " , " + req.originalUrl)
+       
         next();
   }
   );
