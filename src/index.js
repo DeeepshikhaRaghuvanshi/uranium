3511838973c1bdff0b5301fdf5ed837d2a4ffe84
DeeepshikhaRaghuvanshi <ddeepshikha.raghuvanshi@gmail.com>
2022-04-22 07:00:37 +0530

    assignment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Deepshikha:Yash%401603@cluster0.cnmul.mongodb.net/Deepshikha?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
